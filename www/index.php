b2c6cbe4b7236c6ba92e2c9a40b7ccdc27d2b270
root <root@srv005840.cloudip.dk>
2024-02-11 03:50:19 +0000

    FIX 1. edition LAMP server for API

--- www/index.php
+++ www/index.php
@@ -15,24 +15,60 @@ if(isset($_GET['img'])){
 if($_SERVER['REQUEST_URI']=="/benchmark.json"){
   //Save data
   if($_SERVER['REQUEST_METHOD']=="POST"){
-      //Test mariadb
-      //$db=mysqli_connect("127.0.0.1","hardinfo","hardinfo","hardinfo");
-      //mysqli_query($db,"insert into test values ('".file_get_contents("php://input")."','','');");
-
-      //Currently looping through go-server - TODO 
-      $options = array('http' => array(
-      'method' => 'POST',
-      'content' => file_get_contents("php://input"),
-      'header' => "Content-Type: application/octet-stream\r\n"));
-    echo file_get_contents("http://127.0.0.1:90".$_SERVER['REQUEST_URI'],false,stream_context_create($options));
+      //Store JSON in Mariadb
+      $j=json_decode(file_get_contents("php://input"),true,3);
+      $mysqli=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
+      $mysqli->query("insert into test values ('".file_get_contents("php://input")."','".serialize($j)."','DATA');");
+      $SUPER=0;
+      foreach($j as $k=>$v){
+          $stmt=$mysqli->prepare("insert into benchmark_result values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?, unix_timestamp(now()),? );");
+          $stmt->bind_param('sdsssssiiiiisssiiiisdiiis',$k,$v['BenchmarkResult'],$v['ExtraInfo'],$v['MachineId'],$v['Board'],$v['CpuName'],$v['CpuConfig'],$v['NumCpus'],$v['NumCores'],$v['NumThreads'],$v['MemoryInKiB'],$v['PhysicalMemoryInMiB'],$v['MemoryTypes'],$v['OpenGlRenderer'],$v['GpuDesc'],$v['PointerBits'],$SUPER,$v['UsedThreads'],$v['BenchmarkVersion'],$v['UserNote'],$v['ElapsedTime'],$v['MachineDataVersion'],$v['Legacy'],$v['NumNodes'],$v['MachineType']);
+          $stmt->execute();
+      }
   }
-
   //Fetch data
   if($_SERVER['REQUEST_METHOD']=="GET"){
-    //Currently looping through go-server - TODO
-    echo file_get_contents("http://127.0.0.1:90".$_SERVER['REQUEST_URI']);
+      $mysqli=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
+      $d=array();
+      $qbt=$mysqli->query("Select benchmark_type from benchmark_result group by benchmark_type;");
+      while($rbt=$qbt->fetch_array()){
+         $q=$mysqli->query("Select machine_id, extra_info, user_note, machine_type, benchmark_version, AVG(benchmark_result) AS benchmark_result,
+             board, cpu_name, cpu_config, num_cpus, num_cores,
+             num_threads, memory_in_kib, physical_memory_in_mib, memory_types, opengl_renderer,
+             gpu_desc, pointer_bits, data_from_super_user, used_threads,
+             elapsed_time, machine_data_version, legacy, num_nodes
+	     from benchmark_result where benchmark_type='".$rbt[0]."' group by cpu_name");//,pointer_bits;");
+         while($r=$q->fetch_array()){
+	    $a=array();
+	    $a['MachineId']=$r[0];
+	    $a['ExtraInfo']=$r[1];
+	    $a['UserNote']=$r[2];
+	    $a['MachineType']=$r[3];
+	    $a['BenchmarkVersion']=1*$r[4];
+	    $a['BenchmarkResult']=1*$r[5];
+	    $a['Board']=$r[6];
+	    $a['CpuName']=$r[7];
+	    $a['CpuConfig']=$r[8];
+	    $a['NumCpus']=1*$r[9];
+	    $a['NumCores']=1*$r[10];
+	    $a['NumThreads']=1*$r[11];
+	    $a['MemoryInKiB']=1*$r[12];
+	    $a['PhysicalMemoryInMiB']=1*$r[13];
+	    $a['MemoryTypes']=$r[14];
+	    $a['OpenGlRenderer']=$r[15];
+	    $a['GpuDesc']=$r[16];
+	    $a['PointerBits']=1*$r[17];
+	    $a['DataFromSuperUser']=0;//1*$r[18];
+	    $a['UsedThreads']=1*$r[19];
+	    $a['ElapsedTime']=1*$r[20];
+	    $a['MachineDataVersion']=1*$r[21];
+	    $a['Legacy']=1*$r[22];
+	    $a['NumNodes']=1*$r[23];
+            $d[$rbt[0]][]=$a;
+         }
+      }
+      echo json_encode($d);
   }
-  
   exit(0);
 }
 
@@ -82,9 +118,9 @@ Lots of fun technologies to add on to. Interested?<br>
 <?php
 //---- Show results from database ------
    function results($db,$bench,$sort){ 
-     $results = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where benchmark_type="'.$bench.'" group by cpu_name,benchmark_type order by benchmark_type,res '.$sort.';');
+     $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where benchmark_type="'.$bench.'" group by cpu_name,benchmark_type order by benchmark_type,res '.$sort.';');
      $old="";
-     while ($row = $results->fetchArray()) {
+     while ($row = $q->fetch_array()) {
        if($old!=$row[1]) {
          $old=$row[1];
          echo "<tr><td colspan=3>&nbsp;</td></tr>";
@@ -95,8 +131,7 @@ Lots of fun technologies to add on to. Interested?<br>
    }
 
    echo "<tr><td colspan=3><h2>Results from hardinfo database</h2></td></tr>";  
-   //TODO use mariadb instead
-   $db = new SQLite3('../../hardinfo-database.db');
+   $db=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
    echo "<tr><td valign=top><table>"; 
    results($db,"CPU Blowfish (Single-thread)","desc");
    results($db,"CPU Blowfish (Multi-thread)","desc");

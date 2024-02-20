718a7836c3d5a7f82d94487886ce59097c64b497
root <root@srv005840.cloudip.dk>
2024-02-20 14:51:16 +0000

    ADD www-api getbenchmarks

---
+++ www/api/getbenchmarks.php
@@ -0,0 +1,10 @@
+<?php
+//---- get benchmarks ------
+
+  $db=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
+
+  $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result group by cpu_name,benchmark_type order by res '.$sort.';');
+  $r = $q->fetch_all();
+
+  echo json_encode($r);
+?>

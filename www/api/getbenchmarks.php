b04c8e0c2a13327b063048042730fa170ff38c8f
root <root@srv005840.cloudip.dk>
2024-02-20 14:52:18 +0000

    FIX getbenchmark removed $sort

--- www/api/getbenchmarks.php
+++ www/api/getbenchmarks.php
@@ -3,7 +3,7 @@
 
   $db=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
 
-  $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result group by cpu_name,benchmark_type order by res '.$sort.';');
+  $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result group by cpu_name,benchmark_type order by res desc;');
   $r = $q->fetch_all();
 
   echo json_encode($r);

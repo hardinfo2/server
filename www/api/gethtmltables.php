ec51c48c9cd8dc748ccf3ec24f88f17957b67e7d
hwspeedy <ns@bigbear.dk>
2024-03-10 01:37:20 +0000

    FIX moved a graph above tables

--- www/api/gethtmltables.php
+++ www/api/gethtmltables.php
@@ -3,7 +3,6 @@
 //Note this is just an example - please use json instead of html...
 
 
-  echo "<h2>Results from <font color=black>hard<font color=#4060FF>info<font color=red>2</font></font></font> database</h2>";
   $db=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
     $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result group by benchmark_type,cpu_name order by benchmark_type,res desc;');
     $old="";

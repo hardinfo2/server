e5341133dab775a17d4c46c8571be299314d747c
hwspeedy <ns@bigbear.dk>
2024-04-12 15:15:04 +0000

    FIX TopNav added german

--- www/js/translate.js
+++ www/js/translate.js
@@ -5,16 +5,16 @@
 window["translate"]=new Array();
 
 //topnav
-window["translate"].push(["news","en","News"],["news","da","Nyhed"],["news","jp","\u30cb\u30e5\u30fc\u30b9"],["news","es","Noticias"],["news","cn","\u6d88\u606f"]);
+window["translate"].push(["news","en","News"],["news","da","Nyhed"],["news","jp","\u30cb\u30e5\u30fc\u30b9"],["news","es","Noticias"],["news","cn","&nbsp;&nbsp;\u6d88\u606f&nbsp;"],["news","de","Nachricht"]);
 
-window["translate"].push(["bg","en","Bench Graphs <i class='fa fa-caret-down'></i>"],["bg","da","Bench Graf <i class='fa fa-caret-down'></i>"],["bg","jp","\u30d9\u30f3\u30c1\u30b0\u30e9\u30d5 <i class='fa fa-caret-down'></i>"],["bg","es","Banco Gr\u00e1ficos <i class='fa fa-caret-down'></i>"],["bg","cn","\u57fa\u6e96\u5716 <i class='fa fa-caret-down'></i>"]);
+window["translate"].push(["bg","en","Bench Graphs <i class='fa fa-caret-down'></i>"],["bg","da","Bench Graf <i class='fa fa-caret-down'></i>"],["bg","jp","\u30d9\u30f3\u30c1\u30b0\u30e9\u30d5 <i class='fa fa-caret-down'></i>"],["bg","es","Banco Gr\u00e1ficos <i class='fa fa-caret-down'></i>"],["bg","cn","&nbsp;\u57fa\u6e96\u5716&nbsp; <i class='fa fa-caret-down'></i>&nbsp;"],["bg","de","Bankgraf <i class='fa fa-caret-down'></i>"]);
 
-window["translate"].push(["bs","en","Bench Stats <i class='fa fa-caret-down'></i>"],["bs","da","Bench Tal <i class='fa fa-caret-down'></i>"],["bs","jp","\u30d9\u30f3\u30c1\u7d71\u8a08 <i class='fa fa-caret-down'></i>"],["bs","es","Banco n\u00famero<i class='fa fa-caret-down'></i>"],["bs","cn","\u57fa\u6e96\u7d71\u8a08 <i class='fa fa-caret-down'></i>"]);
+window["translate"].push(["bs","en","Bench Stats <i class='fa fa-caret-down'></i>"],["bs","da","Bench Tal <i class='fa fa-caret-down'></i>"],["bs","jp","\u30d9\u30f3\u30c1\u7d71\u8a08 <i class='fa fa-caret-down'></i>"],["bs","es","Banco n\u00famero<i class='fa fa-caret-down'></i>"],["bs","cn","&nbsp;\u57fa\u6e96\u7d71\u8a08&nbsp; <i class='fa fa-caret-down'></i>&nbsp;"],["bs","de","Bankstatistik <i class='fa fa-caret-down'></i>"]);
 
-window["translate"].push(["bc","en","Bench Compare"],["bc","da","Bench Sammenlign"],["bc","jp","\u30d9\u30f3\u30c1\u306e\u6bd4\u8f03"],["bc","es","Banco Comparar"],["bc","cn","\u57fa\u6e96\u6bd4\u8f03"]);
+window["translate"].push(["bc","en","Bench Compare"],["bc","da","Bench Sammenlign"],["bc","jp","\u30d9\u30f3\u30c1\u306e\u6bd4\u8f03"],["bc","es","Banco Comparar"],["bc","cn","&nbsp;\u57fa\u6e96\u6bd4\u8f03&nbsp;"],["bc","de","Bank Vergleich"]);
 
-window["translate"].push(["userguide","en","User Guide"],["userguide","da","Manual"],["userguide","jp","\u30e6\u30fc\u30b6\u30fc\u30ac\u30a4\u30c9"],["userguide","es","Gu\u00eda"],["userguide","cn","\u4f7f\u7528\u8005\u6307\u5357"]);
+window["translate"].push(["userguide","en","User Guide"],["userguide","da","Manual"],["userguide","jp","\u30e6\u30fc\u30b6\u30fc\u30ac\u30a4\u30c9"],["userguide","es","Gu\u00eda"],["userguide","cn","&nbsp;\u4f7f\u7528\u8005\u6307\u5357&nbsp;"],["userguide","de","Handbuch"]);
 
-window["translate"].push(["history","en","History"],["history","da","Historie"],["history","jp","\u6b74\u53f2"],["history","es","Historia"],["history","cn","\u6b77\u53f2"]);
+window["translate"].push(["history","en","History"],["history","da","Historie"],["history","jp","\u6b74\u53f2"],["history","es","Historia"],["history","cn","&nbsp;\u6b77\u53f2&nbsp;"],["history","de","Geschichte"]);
 
-window["translate"].push(["about","en","About"],["about","da","Om"],["about","jp","\u306b\u3064\u3044\u3066"],["about","es","Acerca"],["about","cn","\u95dc\u65bc"]);
+window["translate"].push(["about","en","About"],["about","da","Om"],["about","jp","\u306b\u3064\u3044\u3066"],["about","es","Acerca"],["about","cn","&nbsp;\u95dc\u65bc&nbsp;&nbsp;"],["about","de","Um"]);

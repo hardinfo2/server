1448bc53a659b5e3223dc8877d0b9f8ed002e3ca
hwspeedy <ns@bigbear.dk>
2024-06-21 08:03:40 +0000

    FIX repology run once per 24 hour

--- www/repology.php
+++ www/repology.php
@@ -6,7 +6,7 @@ header('Content-type: image/svg+xml');
 $db=new mysqli("127.0.0.1","hardinfo","hardinfo","hardinfo");
 $r=mysqli_fetch_row($db->query("select value<unix_timestamp(now())-3600*24 from settings where name='repology-refresh'"));
 
-$r[0]=1;
+//$r[0]=1;
 
 if(1*$r[0]){ //refresh
 

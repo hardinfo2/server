27db80df86a113e68c7fd95a8a346507bbe74ab2
hwspeedy <ns@bigbear.dk>
2024-09-01 19:53:27 +0000

    FIX Move all GPU to GPU tests, add note, add GPU to incoming

--- www/api/getbenchmarks.php
+++ www/api/getbenchmarks.php
@@ -6,26 +6,27 @@
   if(!isset($_GET['u'])) $_GET['u']="DESKTOP";
 
   switch($_GET['u']){
-     case "SBC": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where machine_type="Single-board computer" and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     case "SBC": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where machine_type="Single-board computer" and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "ALL": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where not instr(cpu_name,"Sample") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     case "ALL": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where not instr(cpu_name,"Sample") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
 
      case "":
-     case "DESKTOP": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where machine_type="Desktop" and not instr(cpu_name,"XEON") and not instr(cpu_name,"EPYC") and not instr(cpu_name,"Sample") and not instr(cpu_name,"U") and /*not instr(cpu_name,"Celeron") and not instr(cpu_name,"Core 2") and*/ (left(machine_type,7)!="Virtual") and pointer_bits=64 and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     case "DESKTOP": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where machine_type="Desktop" and not instr(cpu_name,"XEON") and not instr(cpu_name,"EPYC") and not instr(cpu_name,"Sample") and not instr(cpu_name,"U") and /*not instr(cpu_name,"Celeron") and not instr(cpu_name,"Core 2") and*/ (left(machine_type,7)!="Virtual") and pointer_bits=64 and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "WORKSERV": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (instr(cpu_name,"XEON") or instr(cpu_name,"EPYC") or (machine_type="Workstation") or (machine_type="Server")) and (left(machine_type,7)!="Virtual") and pointer_bits=64 and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     case "WORKSERV": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (instr(cpu_name,"XEON") or instr(cpu_name,"EPYC") or (machine_type="Workstation") or (machine_type="Server")) and (left(machine_type,7)!="Virtual") and pointer_bits=64 and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "NOTEBOOK": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (machine_type="Notebook" or machine_type="Laptop") and (left(machine_type,7)!="Virtual") and not instr(cpu_name,"XEON") and not instr(cpu_name,"EPYC") and pointer_bits=64 and not instr(benchmark_type,"OpenGL") and pointer_bits=64 group by cpu_name,benchmark_type order by res desc;'); break;
+     case "NOTEBOOK": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (machine_type="Notebook" or machine_type="Laptop") and (left(machine_type,7)!="Virtual") and not instr(cpu_name,"XEON") and not instr(cpu_name,"EPYC") and pointer_bits=64 and not instr(benchmark_type,"GPU") and pointer_bits=64 group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "INTEL": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where instr(cpu_name,"Intel") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") and pointer_bits=64 group by cpu_name,benchmark_type order by res desc;'); break;
+     case "INTEL": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where instr(cpu_name,"Intel") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"GPU") and pointer_bits=64 group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "AMD": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where instr(cpu_name,"AMD") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") and pointer_bits=64 group by cpu_name,benchmark_type order by res desc;'); break;
+     case "AMD": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where instr(cpu_name,"AMD") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"GPU") and pointer_bits=64 group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "32": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where pointer_bits=32 and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     case "32": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where pointer_bits=32 and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
 
-     case "OTHER": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where not instr(cpu_name,"AMD") and not instr(cpu_name,"Intel") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     case "OTHER": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where not instr(cpu_name,"AMD") and not instr(cpu_name,"Intel") and (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
 
-    case "GPU" : $q = $db->query('SELECT IF(INSTR(opengl_renderer,"llvmpipe") OR INSTR(opengl_renderer,"softpipe"),"Software",   LEFT(  REPLACE(REPLACE(REPLACE(opengl_renderer, "(tm)",""),"(TM)",""),"(R)","")  , IF(LOCATE("(",  REPLACE(REPLACE(REPLACE(opengl_renderer, "(tm)",""),"(TM)",""),"(R)","")  )<10,9999,LOCATE("(",  REPLACE(REPLACE(REPLACE(opengl_renderer, "(tm)",""),"(TM)",""),"(R)","")  )-2))) gpuname, benchmark_type, avg(benchmark_result) res FROM benchmark_result WHERE instr(benchmark_type,"GPU") and NOT ISNULL(opengl_renderer) and not instr(machine_type,"irtual") GROUP BY gpuname,benchmark_type order by res DESC;'); break;
+//    case "GPU" : $q = $db->query('SELECT REPLACE(IF(INSTR(opengl_renderer,"llvmpipe") OR INSTR(opengl_renderer,"softpipe"),"Software", LEFT(  REPLACE(REPLACE(REPLACE(opengl_renderer, "(tm)",""),"(TM)",""),"(R)","")  , IF(LOCATE("(",  REPLACE(REPLACE(REPLACE(opengl_renderer, "(tm)",""),"(TM)",""),"(R)","")  )<10,9999,LOCATE("(",  REPLACE(REPLACE(REPLACE(opengl_renderer, "(tm)",""),"(TM)",""),"(R)","")  )-2))),"/PCIe/SSE2","")  gpuname, benchmark_type, avg(benchmark_result) res FROM benchmark_result WHERE instr(benchmark_type,"GPU") and NOT ISNULL(opengl_renderer) and not instr(machine_type,"irtual") GROUP BY gpuname,benchmark_type order by res DESC;'); break;
+     case "GPU" : $q = $db->query('SELECT gpu, benchmark_type, avg(benchmark_result) res FROM benchmark_result WHERE instr(benchmark_type,"GPU") and NOT ISNULL(opengl_renderer) and not instr(machine_type,"irtual") GROUP BY gpu,benchmark_type order by res DESC;'); break;
 
    /*FIXME*/
      case "TOP": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
@@ -33,7 +34,7 @@
      case "BOT": $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (left(machine_type,7)!="Virtual") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
 
      /*User Group*/
-     default: $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (user_note="'.mysqli_real_escape_string($db,$_GET['u']).'") and (left(machine_type,7)!="Virtual") and not instr(cpu_name,"Sample") and not instr(benchmark_type,"OpenGL") group by cpu_name,benchmark_type order by res desc;'); break;
+     default: $q = $db->query('SELECT cpu_name,benchmark_type,avg(benchmark_result) res FROM benchmark_result where (user_note="'.mysqli_real_escape_string($db,$_GET['u']).'") and (left(machine_type,7)!="Virtual") and not instr(cpu_name,"Sample") and not instr(benchmark_type,"GPU") group by cpu_name,benchmark_type order by res desc;'); break;
   }
 
 

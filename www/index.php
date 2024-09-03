d4665028c3cb9c63763121642d9ad800b3fb27d9
hwspeedy <ns@bigbear.dk>
2024-09-03 18:49:39 +0000

    FIX round 2 decimals for app compat

--- www/index.php
+++ www/index.php
@@ -56,7 +56,7 @@ if($_SERVER['SCRIPT_URL']=="/benchmark.json"){
          $grpby="cpu_name";$filter="";
          if(substr($rbt[0],0,11)=="GPU Drawing") {$grpby="GPU";$filter="and (not isnull(GPU) and GPU!='' and not isnull(opengl_renderer) and opengl_renderer!='')";}
          if(substr($rbt[0],0,11)=="GPU OpenGL ") {$grpby="GPU";$filter="and (not isnull(GPU) and GPU!='' and not isnull(opengl_renderer) and opengl_renderer!='')";}
-         $q=$mysqli->query("Select machine_id, extra_info, user_note, machine_type, benchmark_version, round(AVG(benchmark_result),1) AS benchmark_result,
+         $q=$mysqli->query("Select machine_id, extra_info, user_note, machine_type, benchmark_version, round(AVG(benchmark_result),2) AS benchmark_result,
              board, cpu_name, cpu_config, num_cpus, num_cores,
              num_threads, memory_in_kib, physical_memory_in_mib, memory_types, opengl_renderer,
              gpu_desc, pointer_bits, data_from_super_user, used_threads,

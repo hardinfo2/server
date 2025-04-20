cbe361603bd23285b4d6d4960fc41198d3bfb319
hwspeedy <ns@bigbear.dk>
2025-04-20 13:16:32 +0000

    FIX download minor changes

--- setup/update_hardinfo_week.sh
+++ setup/update_hardinfo_week.sh
@@ -23,8 +23,10 @@ curl -s -L https://hardinfo2.org/github?downloadlist |grep "hardinfo2_\|hardinfo
 rm -f /var/www/html/server/www/downloads.ids
 echo "<h1>Debian/APT Based</h1>sudo apt install ./hardinfo2_FULLNAME<br><br>" >/var/www/html/server/www/downloads.ids
 cat /var/www/html/server/www/downloads1.ids |grep '2_2'>> /var/www/html/server/www/downloads.ids
-echo "<h1>Fedora/RPM Based</h1>sudo dnf install ./hardinfo2-FULLNAME<br><br>" >>/var/www/html/server/www/downloads.ids
-cat /var/www/html/server/www/downloads1.ids |grep '2-2'>> /var/www/html/server/www/downloads.ids
+echo "<h1>Fedora/DNF/RPM Based</h1>sudo dnf install ./hardinfo2-FULLNAME<br><br>" >>/var/www/html/server/www/downloads.ids
+cat /var/www/html/server/www/downloads1.ids |grep '2-2'|grep -v SUSE>> /var/www/html/server/www/downloads.ids
+echo "<h1>OpenSuse/ZYPPER/RPM Based</h1>sudo zypper --no-gpg-checks install ./hardinfo2-FULLNAME<br><br>" >>/var/www/html/server/www/downloads.ids
+cat /var/www/html/server/www/downloads1.ids |grep '2-2'|grep SUSE>> /var/www/html/server/www/downloads.ids
 echo "<h1>Arch/PACMAN Based</h1>sudo pacman -U ./hardinfo2-FULLNAME<br><br>" >>/var/www/html/server/www/downloads.ids
 cat /var/www/html/server/www/downloads1.ids |grep -v '2_2' |grep -v '2-2' >> /var/www/html/server/www/downloads.ids
 rm -f /var/www/html/server/www/downloads1.ids

73be3ad43ded23af5d06393bcd2bd8ae8ce2e637
hwspeedy <ns@bigbear.dk>
2026-01-15 13:31:01 +0000

    FIX Updated DB status page coloring to show updated release version

--- www/api/getdbstats.php
+++ www/api/getdbstats.php
@@ -25,16 +25,12 @@ function show_table_color($q){
 	$i=0;
 	while($i<$q->field_count){
             echo "<td class='tableleft'>";
-	    if(($row[0]=="2.2.14")) echo "<font color=blue>";//DEV
-	    else if($row[0]=="2.2.13") {if($row[1]%19==0) echo "<font color=green>";  else echo "<font color=black>";}//LKG REL
-	    else if($row[0]=="2.2.12") {if($row[1]%19==0) echo "<font color=green>";  else echo "<font color=black>";}//LKG <-------This will be minimum
-	    else if($row[0]=="2.2.10") {if($row[1]%18==0) echo "<font color=green>";  else echo "<font color=black>";}//REL 
-	    else if($row[0]=="2.2.9")  {if($row[1]%18==0) echo "<font color=green>";  else echo "<font color=black>";}//pre
-	    else if($row[0]=="2.2.7")  {if($row[1]%18==0) echo "<font color=green>";  else echo "<font color=black>";}//REL
-	    else if($row[0]=="2.2.6")  {if($row[1]%18==0) echo "<font color=green>";  else echo "<font color=black>";}//pre
-	    else if($row[0]=="2.2.4")  {if($row[1]%17==0) echo "<font color=orange>"; else echo "<font color=black>";}//REL
-	    else if($row[0]=="2.2.3")  {if($row[1]%17==0) echo "<font color=orange>"; else echo "<font color=black>";}//pre
-	    else if($row[0]=="2.1.11") {if($row[1]%15==0) echo "<font color=orange>"; else echo "<font color=black>";}//REL
+	    if(($row[0]=="2.2.17")) echo "<font color=blue>";//DEV
+	    else if($row[0]=="2.2.16") {if($row[1]%19==0) echo "<font color=green>";  else echo "<font color=black>";}//REL
+	    else if($row[0]=="2.2.15") {if($row[1]%19==0) echo "<font color=green>";  else echo "<font color=black>";}//pre
+            else if($row[0]=="2.2.13") {if($row[1]%19==0) echo "<font color=green>";  else echo "<font color=black>";}//REL
+	    else if($row[0]=="2.2.12") {if($row[1]%19==0) echo "<font color=green>";  else echo "<font color=black>";}//pre <--This will be minimum
+	    else if($row[0]=="2.2.10") {if($row[1]%18==0) echo "<font color=orange>";  else echo "<font color=black>";}//REL
 	    else echo "<font color=red>";//TOO OLD, Old Development
 	    echo htmlspecialchars($row[$i])."</td>";
 	    $i++;
